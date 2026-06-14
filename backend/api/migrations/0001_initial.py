af7c7effba2a9f217985cb5f0327c736841d17c7
Rostislav <ros.kh.23@gmail.com>
2026-06-15 00:33:04 +0300

    fix PEP8

--- backend/api/migrations/0001_initial.py
+++ backend/api/migrations/0001_initial.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -14,8 +13,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Task',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=120, verbose_name='Заголовок')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True,
+                                           serialize=False,
+                                           verbose_name='ID')),
+                ('title',
+                 models.CharField(max_length=120, verbose_name='Заголовок')),
                 ('description', models.TextField()),
                 ('completed', models.BooleanField(default=False)),
             ],
